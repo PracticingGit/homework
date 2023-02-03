181bc6e31688482861af4b37e9146b974e7e8ee2
DESKTOP-JQDL6G8\theCHAIN <khgm234@gmail.com>
2023-02-04 00:48:43 +0900

    1st summit

--- src/homework_230203.java
+++ src/homework_230203.java
@@ -1,7 +1,7 @@
 public class homework_230203 {
     static class Transportation {
         int number = 0;
-        int charge = 2000;
+        int charge = 1500;
         int fuelTank = 100;
         int speed = 0;
         int speedChange = 0;
@@ -27,11 +27,10 @@ public class homework_230203 {
         }
 
         public void checkDriving() {
-            status = !status;
             if (status = true) {
-                System.out.println("현재 버스는 운행중입니다");
+                System.out.println("현재 : 운행중");
             } else {
-                System.out.println("현재 버스는 차고지행입니다");
+                System.out.println("현재 : 차고지행");
             }
         }
 
@@ -46,31 +45,27 @@ public class homework_230203 {
         public void getPassenger() {
             System.out.println("현재 승객의 수는 " + currentPassenger + "명입니다");
             System.out.println("잔여 객석의 수는 " + (maxPassenger - currentPassenger) + "석입니다");
+        }
+
+        public void getCharge() {
             System.out.println("현재 요금은 " + charge + "원입니다");
         }
 
-//        public void getCharge() {
-//            System.out.println("현재 요금은 " + charge + "원입니다");
-//        }
-
         public void consumeFuel(int consume) {
-            fuelTank -= consume;
-            System.out.println("현재 주유량은 " + fuelTank + "L입니다");
             if (fuelTank - consume < 0) {
                 System.out.println("해당 주파거리는 연료부족으로 주파할 수 없습니다");
             } else if (fuelTank - consume > 0) {
                 fuelTank -= consume;
+                System.out.println("현재 주유량은 " + fuelTank + "L입니다");
                 if (fuelTank - consume <= 10) {
-                    System.out.println("현재 주유량은 " + fuelTank + "L입니다");
                     System.out.println("연료가 부족하므로 차고지로 향합니다");
                     changeDriving();
                 }
-                System.out.println("현재 주유량은 " + fuelTank + "L입니다");
             }
         }
 
-        public void addFuel(int consume) {
-            fuelTank += consume;
+        public void addFuel(int add) {
+            fuelTank += add;
             System.out.println("현재 주유량은 " + fuelTank + "L입니다");
         }
     }
@@ -82,12 +77,11 @@ public class homework_230203 {
         System.out.println("해당 버스의  버스번호는" + bus2.number);
         bus1.addPassenger(2);
         bus1.getPassenger();
-        bus1.getCharge();
+        bus1.getCharge();/* 여기까지 ㅇㅋ */
         bus1.consumeFuel(50);
         bus1.changeDriving();
-        bus1.consumeFuel(10);
+        bus1.addFuel(10);
         bus1.checkDriving();
-        bus1.changeDriving();
         bus1.addPassenger(45);
         bus1.addPassenger(5);
         bus1.getPassenger();
